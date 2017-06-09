0fdf1f868672478cba5a4523a40e7b38cfdb1b44
Ilya Ponetayev <i.ponetaev@ndmsystems.com>
2017-06-09 13:41:01 +0300

    init.c: mute unrelevant warnings

--- src/openvpn/init.c
+++ src/openvpn/init.c
@@ -3052,6 +3052,7 @@ do_option_warnings(struct context *c)
 #endif
         )
     {
+#if !defined(ENABLE_NDM_INTEGRATION)
         if (!o->persist_tun)
         {
             msg(M_WARN, "WARNING: you are using user/group/chroot/setcon without persist-tun -- this may cause restarts to fail");
@@ -3064,6 +3065,7 @@ do_option_warnings(struct context *c)
         {
             msg(M_WARN, "WARNING: you are using user/group/chroot/setcon without persist-key -- this may cause restarts to fail");
         }
+#endif
     }
 
     if (o->chroot_dir && !(o->username && o->groupname))
